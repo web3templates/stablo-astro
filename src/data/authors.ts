e930f56226c4baa0c4e2749fc879d2042b1d2052
Surjith S M <surjithctly@gmail.com>
2022-12-29 21:14:14 +0530

    changes

--- src/data/authors.ts
+++ src/data/authors.ts
@@ -2,7 +2,7 @@ interface Props {
   name: string;
   slug: string;
   image: string;
-  description: string;
+  bio: string;
 }
 
 export const authors: Props[] = [
@@ -10,21 +10,18 @@ export const authors: Props[] = [
     name: "Mario Sanchez",
     slug: "mario-sanchez",
     image: "src/assets/authors/mario.webp",
-    description:
-      "Mario is a Staff Engineer specialising in Frontend at Vercel, as well as being a co-founder of Acme and the content management system Sanity. Prior to this, he was a Senior Engineer at Apple.",
+    bio: "Mario is a Staff Engineer specialising in Frontend at Vercel, as well as being a co-founder of Acme and the content management system Sanity. Prior to this, he was a Senior Engineer at Apple.",
   },
   {
     name: "Joshua Wood",
     slug: "joshua-wood",
     image: "src/assets/authors/joshua.webp",
-    description:
-      "Joshua is a Microsoft Azure Certified Cloud Professional and a Google Certified Associate Cloud Engineer. A Data Analytics at Acme, specializing in the use of cloud infrastructure for Machine Learning and Deep Learning operation at scale.",
+    bio: "Joshua is a Microsoft Azure Certified Cloud Professional and a Google Certified Associate Cloud Engineer. A Data Analytics at Acme, specializing in the use of cloud infrastructure for Machine Learning and Deep Learning operation at scale.",
   },
   {
     name: "Erika Oliver",
     slug: "erika-oliver",
     image: "src/assets/authors/erika.webp",
-    description:
-      "Erika Oliver is a successful entrepreuner. She is the founder of Acme Inc, a bootstrapped business that builds affordable SaaS tools for local news, indie publishers, and other small businesses.",
+    bio: "Erika Oliver is a successful entrepreuner. She is the founder of Acme Inc, a bootstrapped business that builds affordable SaaS tools for local news, indie publishers, and other small businesses.",
   },
 ];
--- src/pages/blog/how-to-become-frontend-master.md
+++ src/pages/blog/how-to-become-frontend-master.md
@@ -2,7 +2,7 @@
 title: "How to become a Frontend Master"
 excerpt: "Ornare cum cursus laoreet sagittis nunc fusce posuere per euismod dis vehicula a, semper fames lacus maecenas dictumst pulvinar neque enim non potenti. Torquent hac sociosqu eleifend potenti."
 publishDate: "2022-11-07T15:39:36.050Z"
-image: "https://images.unsplash.com/photo-1667372393119-3d4c48d07fc9?&fit=crop&w=430&h=240"
+image: "https://images.unsplash.com/photo-1667372393119-3d4c48d07fc9"
 category: "travel"
 author: "erika-oliver"
 layout: "@layouts/BlogLayout.astro"
