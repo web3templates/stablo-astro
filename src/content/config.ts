9d554ae6071f93cd59ccae98cb73659a0e3a5090
Surjith S M <surjithctly@gmail.com>
2022-12-30 20:44:29 +0530

    make draft optional

--- src/content/config.ts
+++ src/content/config.ts
@@ -6,7 +6,7 @@ const BlogPosts = defineCollection({
     excerpt: z.string(),
     category: z.string().trim(),
     author: z.string().trim(),
-    draft: z.boolean(),
+    draft: z.boolean().optional(),
     tags: z.array(z.string()),
     image: z.string().optional(),
     publishDate: z.string().transform((str) => new Date(str)),
