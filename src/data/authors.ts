445971f0f9e2bdbfd5184a8a44dce0d4945afd30
Surjith S M <surjithctly@gmail.com>
2022-12-31 14:20:42 +0530

    fix type & accessibility

--- src/data/authors.ts
+++ src/data/authors.ts
@@ -1,9 +1,11 @@
-interface Props {
+export interface Props {
   name: string;
   slug: string;
   image: string;
   bio: string;
 }
+
+export type Author = Props;
 
 export const authors: Props[] = [
   {
--- src/data/category.ts
+++ src/data/category.ts
@@ -1,9 +1,10 @@
-interface Props {
+export interface Props {
   title: string;
   slug: string;
   color: "green" | "blue" | "orange" | "purple" | "pink";
   description: string;
 }
+export type Category = Props;
 
 export const categories: Props[] = [
   {
