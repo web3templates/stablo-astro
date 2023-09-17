d7fb2c623138172d459d0965a0e3e7c16550620b
DESKTOP-ES692IG\dkcf <danielcalatayudf@gmail.com>
2023-09-17 21:13:54 +0200

    admin fix

--- src/data/menu.ts
+++ src/data/menu.ts
@@ -3,11 +3,15 @@ import path from 'path';
 import yaml from 'js-yaml';
 
 // Define the Menu interface
+export interface MenuItem {
+  label: string;
+  href: string;
+  external?: boolean;
+  badge?: string;
+}
+
 export interface Menu {
-    label: string;
-    href: string;
-    external?: boolean;
-    badge?: string;
+  'menu-items': MenuItem[];
 }
 
 // Read the YAML data from the menu.yml file
@@ -15,7 +19,8 @@ const menuFilePath = path.join(process.cwd(), 'src/data/menu.yml');
 const menuData = fs.readFileSync(menuFilePath, 'utf8');
 
 // Parse the YAML data to get the menu array
-const menu: Menu[] = yaml.load(menuData) as Menu[];
+const menuYAML: Menu = yaml.load(menuData) as Menu;
+const menu: MenuItem[] = menuYAML['menu-items'];
 
 // Export the menu array for use in other parts of your application
 export default menu;
