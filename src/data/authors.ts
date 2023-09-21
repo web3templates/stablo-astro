f1f8fc7b2fe98b1c5cfc0d114058dbb9c01406f7
Yuto2023 <yuto.k753@icloud.com>
2023-09-21 14:23:07 +0900

    わぁ・・・・

--- src/data/authors.ts
+++ src/data/authors.ts
@@ -11,7 +11,7 @@ export const authors: Props[] = [
   {
     name: "神崎優人",
     slug: "mario-sanchez",
-    image: "",
+    image: "./src/assets/authors/6nlg3sej.png",
     bio: "Mario is a Staff Engineer specialising in Frontend at Vercel, as well as being a co-founder of Acme and the content management system Sanity. Prior to this, he was a Senior Engineer at Apple.",
   },
   {
